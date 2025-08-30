c2d6be12cc86bdab7d3021b5132351a9ba27e3f6
Cloudproject2500 <kimkang1738@gmail.com>
2025-08-30 16:51:49 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_62smCEydo'
+        userPoolClientId: '3u2jtojav7i5v8g87hosfhtsjl'
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
