3ab0e3e1d1bcd6b58c953191256f3a6e91155822
Cloudproject2500 <kimkang1738@gmail.com>
2025-08-30 17:02:02 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_62smCEydo'
-        userPoolClientId: '3u2jtojav7i5v8g87hosfhtsjl'
+        userPoolId: 'us-east-1_DX14lDf2o'
+        userPoolClientId: '3i6bjkjtt4hs6e3a7djlrm5ter'
         region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
